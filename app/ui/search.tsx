630d02095fd1afec875baf94a05c667e2ae8eba6
Ian Pollack <ianjpollack@gmail.com>
2024-09-11 16:14:36 -0700

    added pagination

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -9,12 +9,9 @@ export default function Search({ placeholder }: { placeholder: string }) {
   const pathname = usePathname();
   const { replace } = useRouter();
 
-// function handleSearch(term: string) {
-const handleSearch = useDebouncedCallback((term) => {
-
-  console.log(`Searching... ${term}`);
-  
+const handleSearch = useDebouncedCallback((term) => {  
   const params = new URLSearchParams(searchParams);
+  params.set('page', '1');
   if (term) {
     params.set('query', term);
   } else {
@@ -22,7 +19,6 @@ const handleSearch = useDebouncedCallback((term) => {
   }
   replace(`${pathname}?${params.toString()}`);
 }, 300);
-// }
 
   return (
     <div className="relative flex flex-1 flex-shrink-0">
